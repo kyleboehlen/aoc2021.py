ef3a17fa3b5fba3b08389e43dd55966c3ac530b3
Kyle Boehlen <kyleboehlen@gmail.com>
2023-09-05 13:53:47 -0600

    Day 4 wip

---
+++ scripts/4.py
@@ -0,0 +1,55 @@
+import re
+
+def solvePuzzle(input):
+    # Set up data structure for storing the boards and numbers
+    boardNums = {}
+    boards = {} 
+    boardCount = -1
+    y = 0
+    for index, line in enumerate(input):
+        if index == 0:
+            inputNums = line.split(",")
+            continue
+
+        if len(line) <= 1:
+            y = 0
+            boardCount += 1
+            boardNums[boardCount] = []
+            continue
+
+        formattedLine = line.strip()
+        formattedLine = re.sub(r"\s+", ",", formattedLine)
+        for x, num in enumerate(formattedLine):
+            inputArray = [boardCount, x, y]
+            if num not in boards:
+                boards[num] = []
+            boards[num].append(inputArray)
+            boardNums[boardCount].append(num)
+        y += 1
+
+    markedNums = []
+    boardTracker = {}
+    checksumFound = False
+    print(boards)
+    for num in inputNums:
+        if checksumFound:
+            break
+        markedNums.append(num)
+        for b in boards[num]:
+            if b[0] not in boardTracker:
+                boardTracker[b[0]] = [[0] * 5, [0] * 5]
+            boardTracker[b[0]][0][b[1]] += 1
+            boardTracker[b[0]][1][b[2]] += 1
+            if boardTracker[b[0]][0][b[1]] == 5 or boardTracker[b[0]][1][b[2]]:
+                checksum = 0
+                for bNum in boardNums[b[0]]:
+                    if bNum not in markedNums:
+                        checksum += bNum
+                checksum *= num
+                print(checksum)
+                checksumFound = True
+                break
+    
+
+
+    
